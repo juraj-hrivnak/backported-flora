b28bf3c313988a654759a41ccee6aa0bc1004e83
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-01-28 22:59:54 +0100

    Bump version

--- src/main/kotlin/azmalent/backportedflora/BackportedFlora.kt
+++ src/main/kotlin/azmalent/backportedflora/BackportedFlora.kt
@@ -29,7 +29,7 @@ import java.io.File
 object BackportedFlora {
     const val MODID = "backportedflora"
     const val NAME = "Backported Flora"
-    const val VERSION = "1.7"
+    const val VERSION = "1.8"
     const val CONFIG_FILE_NAME = "$MODID.cfg"
 
     const val ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
